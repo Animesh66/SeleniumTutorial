4b365655f769bff27a332cb6125b3a44f14e48d3
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-20 21:10:30 +0530

    new python excel operation files added

---
+++ xlutiles.py
@@ -0,0 +1,28 @@
+import openpyxl as xl
+
+
+def get_row_count(file,sheet_name):  # this function will return the row count of the cell
+    workbook = xl.load_workbook(file)
+    sheet = workbook[sheet_name]
+    return sheet.max_row
+
+
+def get_column_count(file, sheet_name):  # this function will return the column count of the cell
+    workbook = xl.load_workbook(file)
+    sheet = workbook[sheet_name]
+    return sheet.max_column
+
+
+def read_data(file, sheet_name, row_count, column_count):  #  this function will read the value of the cell from excel file
+    workbook = xl.load_workbook(file)
+    sheet = workbook[sheet_name]
+    cell = sheet.cell(row_count,column_count)
+    return cell.value
+
+
+def write_data(file, sheet_name, row_count, column_count, cell_value):  # this function will write data in excel
+    workbook = xl.load_workbook(file)
+    sheet = workbook[sheet_name]
+    cell = sheet.cell(row_count, column_count)
+    cell_value = cell.value
+    return cell_value
