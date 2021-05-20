e32366a3c2814fffce008de363aea9f7c57074d3
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-20 21:12:39 +0530

    new python excel operation files added

--- xlutiles.py
+++ xlutiles.py
@@ -24,5 +24,5 @@ def write_data(file, sheet_name, row_count, column_count, cell_value):  # this f
     workbook = xl.load_workbook(file)
     sheet = workbook[sheet_name]
     cell = sheet.cell(row_count, column_count)
-    cell_value = cell.value
-    return cell_value
+    cell.value = cell_value
+    workbook.save(file)
