dbab8ac7911093b1cc18197e60ad5651b0f992bc
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-22 23:13:12 +0530

    Test Suite test cases

---
+++ test_suite/functional_test/TC_PaymentReturnbyBank.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class PaymentReturnbyBank(unittest.TestCase):
+    def test_paymentreturnbyBank(self):
+        print("Payment returns by bank")
+        self.assertTrue(True, "Test case is failed")
+
+
+if __name__ == "__main__":
+    unittest.main()
