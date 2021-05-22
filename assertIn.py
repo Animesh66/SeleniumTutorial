704031e4d595b1b85fd3e2c1f36621b7cddfae7c
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-22 20:27:32 +0530

    assertion method

---
+++ assertIn.py
@@ -0,0 +1,14 @@
+import unittest
+
+class AsertionIn(unittest.TestCase):
+    def test_assertion_in(self):
+        list_name = {"Python", "Selenium", "Java"}
+        # asserIn will check and return true if the value is present in the list/dictionary/tuple or not
+        self.assertIn("Python", list_name, "This value is not present in the list")
+        # asserIn will check and return true if the value is NOT present in the list/dictionary/tuple or not
+        self.assertNotIn("Python123", list_name, "This value is present in the list")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
