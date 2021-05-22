05e3ee12961732561af5a31a2ce8928a0a3f954f
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-22 14:24:42 +0530

    unit test framework

---
+++ unittestframework.py
@@ -0,0 +1,8 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def first_test_case(self):
+        print("This is my first test case")
+
+if __name__ == "__main__": # this command will execute the untitest case
+    unittest.main
