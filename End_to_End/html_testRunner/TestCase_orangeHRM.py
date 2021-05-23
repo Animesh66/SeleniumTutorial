19c66a567e6363ee127be422f8188db9546749a5
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-23 20:51:44 +0530

    End to End test case(HTML report)

--- End_to_End/html_testRunner/TestCase_orangeHRM.py
+++ End_to_End/html_testRunner/TestCase_orangeHRM.py
@@ -8,15 +8,33 @@ class OrangeHRMTest(unittest.TestCase):  # this is the unittest class
 
     @classmethod # this is a decorator
     def setUpClass(cls):  # this method is called only once before calling any other method
+        print("Test Started...")
         cls.driver = webdriver.Chrome()
         cls.driver.maximize_window()
 
     @classmethod
-    def tearDownClass(cls): # this method is called only once after calling any other method
+    def tearDownClass(cls):  # this method is called only once after calling any other method
         cls.driver.quit()
+        print("Test Completed...")
 
     def test_home_page_title_verification(self):  # this method will verify the page title
         self.driver.get("https://opensource-demo.orangehrmlive.com/")
-        self.driver.set_page_load_timeout(10)
+        self.driver.set_page_load_timeout(15)
         self.page_title = self.driver.title
-        self.assertEqual("orangeHRM", self.page_title, "The page title does not match")
+        self.assertEqual("OrangeHRM", self.page_title, "The page title does not match")
+
+    def test_login(self):  # this  method will verify the login process
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.username_box = self.driver.find_element_by_id("txtUsername")
+        self.username_box.send_keys("Admin")
+        self.password_box = self.driver.find_element_by_id("txtPassword")
+        self.password_box.send_keys("admin123")
+        self.password_box.submit()
+        time.sleep(3)
+        self.page_title = self.driver.title
+        self.assertEqual("OrangeHRM", self.page_title, "The page title does not match")
+
+
+if __name__ == "__main__":  # this command will execute the all class methods at once
+    unittest.main()
+
