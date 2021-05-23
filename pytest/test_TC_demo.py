1ae707e34a8ff775c285bb0ee1c3223470844752
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-23 15:19:36 +0530

    pytest file created.

---
+++ pytest/test_TC_demo.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+def test_tc_demo():  # IMPORTANT: every method name should always start with keyword "test"
+    print("This is demo pytest method")
+
+
+def test_tc2_demo():  # IMPORTANT: every method name should always start with keyword "test"
+    print("This is anther pytest test method")
+
+# To run any pytest file we have to execute it in terminal with command "pytest -v -s"
+# We cannot run any pytest file directly from Pycharm
