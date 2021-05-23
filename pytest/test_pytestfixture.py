52369214e263c1fc9ca329450cfa277ed2c69314
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-23 17:30:00 +0530

    Pytest fixture file added.

---
+++ pytest/test_pytestfixture.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture()  # this decorator is acting as fixture. It will be executed once before every method
+def setup():
+    print("Execute once before every method")
+
+
+def test_method_one(setup):
+    print("This is test method one")
+
+
+def test_method_two(setup):
+    print("This is test method two")
+
+# To run this file we need to type "pytest -v -s pytest/test_pytestfixture.py" in the terminal
