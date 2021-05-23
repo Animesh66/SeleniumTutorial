733e46e87f52529e664d5b206d7fc14c8b11fb18
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-23 17:34:28 +0530

    Pytest fixture file added.

--- pytest/test_pytestfixture.py
+++ pytest/test_pytestfixture.py
@@ -6,11 +6,11 @@ def setup():
     print("Execute once before every method")
 
 
-def test_method_one(setup):
+def test_method_one(setup):  # this method will execute after setup method
     print("This is test method one")
 
 
-def test_method_two(setup):
+def test_method_two(setup):  # this method will execute after setup method
     print("This is test method two")
 
 # To run this file we need to type "pytest -v -s pytest/test_pytestfixture.py" in the terminal
