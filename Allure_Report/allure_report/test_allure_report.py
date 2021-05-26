0a667caf0ee603bdde7d37321e9280cbc21a0c79
Animesh <er.animesh6@hotmail.com>
2021-05-26 23:04:19 +0530

    Allure Report

--- Allure_Report/allure_report/test_allure_report.py
+++ Allure_Report/allure_report/test_allure_report.py
@@ -1,3 +1,16 @@
 from selenium import webdriver
 import pytest
 import allure
+
+class TestHRM():
+
+    def test_logo(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.set_page_load_timeout(10)
+
+    def test_listemployees(self):
+
+
+    def test_login(self):
