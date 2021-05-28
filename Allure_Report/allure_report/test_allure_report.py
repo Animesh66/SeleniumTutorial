054f0634fd8a0e58b71085a1c6a069eaf49013a3
Animesh <er.animesh6@hotmail.com>
2021-05-28 21:05:57 +0530

    Allure Report almost completed

--- Allure_Report/allure_report/test_allure_report.py
+++ Allure_Report/allure_report/test_allure_report.py
@@ -1,10 +1,15 @@
+import allure
+from allure_commons.types import AttachmentType
 from selenium import webdriver
-# import allure
 import pytest
 import time
 
+
+# this decorator will set the severity of the class it can be NORMAL, BLOCKER, CRITICAL or MINOR
+@allure.severity(allure.severity_level.NORMAL)
 class TestHRM():
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_logo(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
@@ -17,9 +22,11 @@ class TestHRM():
             assert False
         self.driver.close()
 
+    @allure.severity(allure.severity_level.NORMAL)
     def test_list_employees(self):
         pytest.skip("Will be implemented later")
 
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_login(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
@@ -34,6 +41,9 @@ class TestHRM():
             self.driver.close()
             assert True
         else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="test_login_screen",
+                          attachment_type=AttachmentType.PNG)
+            # the above statement will capture the screenshot and save it by the name and attachment type will be PNG
             self.driver.close()
             assert False
 
